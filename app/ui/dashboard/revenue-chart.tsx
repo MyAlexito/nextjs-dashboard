589e5066bca6f054cdcf558d192efb4526688aed
MyAlexito <alexcanrey99@gmail.com>
2025-10-20 00:12:17 -0600

    Implementación de streaming con Suspense para mejorar la experiencia del usuario en el panel

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,6 +1,7 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
 import { Revenue } from '@/app/lib/definitions';
 
 // This component is representational only.
@@ -13,10 +14,12 @@ export default async function RevenueChart({
   revenue,
 }: {
   revenue: Revenue[];
-}) {
-  const chartHeight = 350;
-  // NOTE: Uncomment this code in Chapter 7
+}) 
 
+  export default async function RevenueChart() { // Make component async, remove the props
+  const revenue = await fetchRevenue(); // Fetch data inside the component
+
+  const chartHeight = 350;
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
  if (!revenue || revenue.length === 0) {
